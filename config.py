c5c540b11388096926df21369253ead7ffe6be4e
ksyukhen33 <157539437+ksyukhen33@users.noreply.github.com>
2024-03-01 21:29:23 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = "6432216499:AAHfN0-Mrk7DXSr39TWntghCwsHtjfw4n1U"
+
+keys={'доллар':'USD',
+      'евро':'EUR',
+      'рубль':'RUB',
+}
---
+++ extensions.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from config import keys
+class APIException(Exception):
+    pass
+
+class CriptoConverter:
+    @staticmethod
+    def get_price(base:str, quote:str, amount:str):
+
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total = json.loads(r.content)[keys[base]]
+
+        total_base=total*amount
+
+        return total_base
